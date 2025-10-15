23a6e847186d359165dda03230de6aece8295644
Arthur <nashcassano8@gmail.com>
2025-10-15 17:23:22 -0300

    inicializando o banco de dados sqlite

--- SistemaFinanceiro/Program.cs
+++ SistemaFinanceiro/Program.cs
@@ -1,9 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using SistemaFinanceiro.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddDbContext<AppDbContext>(o => o.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
