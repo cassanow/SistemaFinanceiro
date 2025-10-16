05953f7c4a6e92268e503870db29cd11c0676b68
Arthur <nashcassano8@gmail.com>
2025-10-16 13:48:28 -0300

    adicionando interface e repository de cliente

---
+++ SistemaFinanceiro/SistemaFinanceiro.Application/Interfaces/IClienteRepository.cs
@@ -0,0 +1,16 @@
+﻿using SistemaFinanceiro.SistemaFinanceiro.Domain.Entities;
+
+namespace SistemaFinanceiro.SistemaFinanceiro.Application.Interfaces;
+
+public interface IClienteRepository
+{
+    Task<bool> Add(Cliente cliente);
+    
+    Task<bool> Update(Cliente cliente);
+    
+    Task<bool> Delete(Cliente cliente);
+    
+    Task<Cliente> GetById(int id);
+    
+    Task<bool> ClienteExists(int id);
+}
---
+++ SistemaFinanceiro/SistemaFinanceiro.Infrastructure/Repository/ClienteRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SistemaFinanceiro.Context;
+using SistemaFinanceiro.SistemaFinanceiro.Application.Interfaces;
+using SistemaFinanceiro.SistemaFinanceiro.Domain.Entities;
+
+namespace SistemaFinanceiro.SistemaFinanceiro.Infrastructure.Repository;
+
+public class ClienteRepository : IClienteRepository
+{
+    private readonly AppDbContext _context;
+
+    public ClienteRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+    
+    public async Task<bool> Add(Cliente cliente)
+    {
+        _context.Cliente.Add(cliente);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<bool> Update(Cliente cliente)
+    {
+        var retorno = _context.Entry(cliente).State == EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return retorno;
+    }
+
+    public async Task<bool> Delete(Cliente cliente)
+    {
+        _context.Cliente.Remove(cliente);
+        await _context.SaveChangesAsync();
+        return true;
+    }
+
+    public async Task<Cliente> GetById(int id)
+    {
+        return await _context.Cliente.Where(c => c.Id == id).FirstOrDefaultAsync();
+    }
+
+    public async Task<bool> ClienteExists(int id)
+    {
+        return await _context.Cliente.AnyAsync(c => c.Id == id);
+    }
+}
